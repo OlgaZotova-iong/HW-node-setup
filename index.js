140815ea01b2b908557f713b59a3d93955bcd6ea
Olga Zotova <olgazotova1976@yandex.ru>
2026-03-02 14:02:40 +0300

    Plugins are configured to work

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const func = () => {
   const sum = (a, b) => a + b;
   let x = 10;
   let y = 20;
+  const oldVar = 50;
 
   console.log(sum(x, y)); // 30
   debugger;
   x = 20;
   y = 30;
+
+  const z = 100;
+
+  if (x == y) {
+    console.log('equal');
+  }
 
   console.log(sum(x, y)); // 50
 };
